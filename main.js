d5138fd28d6fa4ff95828b3c8c1c945a8c6433e5
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2024-05-23 11:53:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const gravidade = 9.81;
+let tempo= prompt("informe o tempo em segur...")
+let distancia =(0,5 * gravidade*tempo*tempo)   //gravidade vezes tempo vezes o tempo
+alert(Math.floor(distancia))      //essa linha é pra arendondar math.floor
